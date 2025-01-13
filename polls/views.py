f83cf8a54ae0eddc0dc360cdc221310e31d5ec67
LdeLudwig <borgesbfx@gmail.com>
2025-01-13 11:33:53 -0400

    changing views to use written tests

--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,30 @@
 from django.db.models import F
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
 from .models import Question, Choice
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__ltd=timezone.now())
 
-def results(request, question_id):
-    question_id = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question_id})
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
